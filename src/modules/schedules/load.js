af1a7f56196946e31fda28e98e560fad6775ec40
Matheus-Souza97 <matheus-souza1@outlook.com.br>
2026-05-25 11:38:09 -0300

    Criado o service para capturar os agendamentos do dia

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,16 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
 
 //Seleciona o input da data
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   //Obtém a tada do input
   const date = selectedDate.value;
+
+  //Buscar na API os agendamentos
+  const dailySchedules = await scheduleFetchByDay({ date });
+  console.log(dailySchedules);
 
   //Renderia as horas disponíveis
   hoursLoad({ date });
